7526690ff14fa6da70992dcc9d20c3a4b96129d1
Mustafa Mehmedoglu <mikbalm@gmail.com>
2025-03-06 06:11:01 -0500

    class structure initial commit

---
+++ lib/models/model_1.py
@@ -0,0 +1,15 @@
+class Trail:
+    
+    def __init__(self, name):
+        self.name = name
+
+class Hike:
+    
+    def __init__(self, name):
+        self.name = name
+    
+class Hiker:
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
