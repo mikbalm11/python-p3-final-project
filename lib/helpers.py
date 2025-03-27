5c45b96ac3c5fc376c958aec90e6522d35d9c6d8
Mustafa Mehmedoglu <mikbalm@gmail.com>
2025-03-27 07:26:56 -0400

    testing print alls for classes

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,24 +1,37 @@
 # lib/helpers.py
 
+import random
+
 from models.model_1 import Trail
 from models.model_1 import Hiker
 from models.model_1 import Hike
 
-t = Trail("Test trail")
-h = Hiker("John Doe", 30)
+# Create 20 Trail instances
+trail_names = ["Sunset Ridge", "Rocky Path", "Pine Valley", "Bear Creek", "Eagle Summit", "Hidden Falls", "Wildflower Trail", "Misty Woods", "Canyon Loop", "Maple Pass", "Silver Lake", "Golden Peak", "Shadow Mountain", "Crystal Springs", "Lone Pine", "Blue Ridge", "Thunder Pass", "Highland Trek", "Serenity Trail", "Redwood Route"]
+trails = [Trail(name) for name in trail_names]
+
+# Create 20 Hiker instances
+hiker_names = ["John", "Emily", "Mike", "Sarah", "David", "Laura", "Chris", "Anna", "James", "Sophia", "Daniel", "Olivia", "Matthew", "Emma", "Andrew", "Isabella", "Ryan", "Mia", "Ethan", "Charlotte"]
+hikers = [Hiker(name, random.randint(18, 60)) for name in hiker_names]
+
+# Create 20 Hike instances, randomly assigning trails and hikers
+hikes = [Hike(random.choice(trails), random.choice(hikers)) for _ in range(20)]
+
+# t = Trail("Test trail")
+# h = Hiker("John Doe", 30)
+# k = Hike(t, h)
 
 def helper_1():
     print("Performing useful function#1.")
 
 def trailname():
-    print(t)
+    print(trails)
 
 def hikername():
-    print(h)
+    print(hikers)
 
 def hikename():
-    hike = Hike(t, h)
-    print(hike)
+    print(hikes)
 
 def exit_program():
     print("Goodbye!")
--- lib/models/model_1.py
+++ lib/models/model_1.py
@@ -1,25 +1,96 @@
 class Trail:
     
+    all =[]
+    
     def __init__(self, name):
         self.name = name
+        Trail.all.append(self)
     
-    def __str__(self):
+    def __repr__(self):
         return f"Trail: {self.name}"
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and 3 <= len(new_name) and not hasattr(self, "name"):
+            self._name = new_name
+        else:
+            raise Exception("Trail name must be a string longer than 3 characters.")\
+    
+    def hikes(self):
+        return [hike for hike in Hike.all if hike.trail is self]
+
+    def hikers(self):
+        return list({hike.hiker for hike in self.hikes()})
+    
+    def all_trails(self):
+        return Trail.all
 
 class Hike:
+    
+    all = []
     
     def __init__(self, trail, hiker):
         self.trail = trail
         self.hiker = hiker
+        Hike.all.append(self)
 
-    def __str__(self):
+    def __repr__(self):
         return f"Hike: {self.trail} completed by {self.hiker}"
+
+    @property
+    def hiker(self):
+        return self._hiker
+    
+    @hiker.setter
+    def hiker(self, value):
+        if isinstance(value, Hiker):
+            self._hiker = value
+        else:
+            raise Exception("Hiker must be an instance of the Hiker class.")
+    
+    @property
+    def trail(self):
+        return self._trail
+    
+    @trail.setter
+    def trail(self, value):
+        if isinstance(value, Trail):
+            self._trail = value
+        else:
+            raise Exception("Trail must be an instance of the Trail class.")
+    
+    def all_hikes(self):
+        return Hike.all
     
 class Hiker:
+    
+    all = []
     
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        Hiker.all.append(self)
 
-    def __str__(self):
+    def __repr__(self):
         return f"Hiker: {self.name}, {self.age} years old"
+    
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and 1 <= len(new_name) <= 15:
+            self._name = new_name
+        else:
+            raise Exception("Hiker name must be a string between 1 and 15 characters.")
+    
+    def hikes(self):
+        return [hike for hike in Hike.all if hike.hiker is self]
+    
+    def all_hikers(self):
+        return Hiker.all
